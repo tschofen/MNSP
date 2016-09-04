8cd1634b77b143d93ba2637e449ea08c08803ae2
Martin Tschofen <mtschofen@olson.com>
2016-09-04 16:48:14 -0500

    Added email send with scheduling -- controlled by SCHEDULED_PARKS config.variable

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var request = require("request");
 var rp = require('request-promise');
 var fs = require('fs');
 var formidable = require("formidable");
+var nodemailer = require('nodemailer');
+var schedule = require('node-schedule');
 var prep = require('./setData');
 var view = require('./renderResults');
 
@@ -16,9 +18,10 @@ const PORT=8082;
 const BASE = 'https://reservations.usedirect.com/MinnesotaWebHome/Accessible/AvailableUnitsADA.aspx?';
 //var LISTING = BASE + 'place_id=70&arrivalDate=9/8/2016&nights=2'; 
 
+var params;
 
 function handleRequest(request, response) {
-	var params = url.parse(request.url, true).query;
+	params = url.parse(request.url, true).query;
 
 	/*simple authentication via params*/
 	if(process.env.LOCALENV == 'true'){
@@ -42,6 +45,10 @@ function handleRequest(request, response) {
 				html += data; //pass in the empty form again
 				//form was submitted so get the result data and output after the async scrape call
 				outputData(request, response, html);
+			} else if(request.method.toLowerCase() == 'get' && (params.startDate || params. startdate)){
+				html += data; //pass in the empty form again
+				outputEmail(request, response, html, params);
+				//response.end('Blastomated');
 			} else {
 				html += prep.set(data);
 				//output the form without any results
@@ -51,121 +58,189 @@ function handleRequest(request, response) {
 	});
 }
 
+function outputEmail(request, response, html, fields){
+	processData(request, response, html, fields, 'email');
+}
+
 //We need a function which handles requests and send response
 function outputData(request, response, html){
 	var form = new formidable.IncomingForm();
-	form.parse(request, function (err, fields) {	
-		var cheerio = require('cheerio'); // Basically jQuery for node.js
-
-		response.write(prep.set(html, fields)); //fill in the form fields from previous request
-		html = '<div class="container"><h2>Search Results: '+fields.park.split("|")[1]+'</h2><!-- RESULTS --></div>';
-
-		var formErrors = [];
-		//bad form entries
-		if(fields.startDate == ""){
-			formErrors.push("When, oh when!");
-		}
-		if(fields.nights < 1){
-			fields.nights = "1";
-		}
-		if(fields.week < 1){
-			fields.week = "1"
-		}
-		if(formErrors.length > 0){
-			html = prep.setResults(html, formErrors.join(""));
-			response.write(html);
-			response.end();
-			return;
-		}
-		var d = new Date(fields.startDate);
-
-		var options = {
-				uri: BASE + 'place_id=' + fields.park.split("|")[0] + '&arrivalDate=' + fields.startDate + '&nights=' + fields.nights,
-				headers: {
-					'User-Agent': 'request'
-				},
-				transform: function (body) {
-					return cheerio.load(body);
-				},
-				loc: fields.park.split("|")[1],
-				date: fields.startDate,
-				day: d.getDay(),
-				nights: Number(fields.nights)
-		};
-		//url params are getting clobbert
-		options.exturl = options.uri;
-
-		var urls = [Object.assign({}, options)];
-
-		//no need to process if only 1
-		for (var i = fields.week - 1; i > 0; i--) {
-			d.setDate(d.getDate() + 7);
-			var nextDate = (d.getMonth()+1)+'/'+ d.getDate() +'/'+d.getFullYear();
-			options.uri = BASE + 'place_id=' + fields.park.split("|")[0] + '&arrivalDate=' + nextDate + '&nights=' + fields.nights;
-			options.loc = fields.park.split("|")[1];
-			options.date = (d.getMonth()+1)+'/'+ d.getDate() +'/'+d.getFullYear();
-			options.day = d.getDay();
-			options.exturl = options.uri;
-			//clone the object
-			urls.push(rp(Object.assign({}, options)));
-		}
-		// console.log('options', urls);
-
-
-		/*now loop through all the urls and get the data*/
-		var listings = [];
-
-		/*process all calls in parallel*/
-		function async(options, callback) {
-			rp(options)
-			.then(function ($) {
-				var list = [];
-				//console.log($.html());
-				$('.UnitresultDiv').find('div > a').each(function (index, element) {
-					list.push($(element).text());
-				});
-				listings.push(view.render(options, list));
-				callback(options);
-			})
-			.catch(function (err) {
-				// Crawling failed or Cheerio choked...
-				console.log('trouble', err.name, err.statusCode); //full text: err.error
-				var os = {loc: options.loc, day: options.day, date: options.date};
-				listings.push(view.render(os));
-				results.push({}); //results don't increase without this. Not sure why
-				//console.log('results count', results.length);
-				if(results.length == urls.length){
-					html = prep.setResults(html, listings.join(""));
-					response.write(html);
-				}
-			});
-		}
-		function final() { 
-			console.log('Done', results.length);
-			html = prep.setResults(html, listings.join(""));
-			response.write(html);
-			response.end();
-		}
-
-		var results = [];
-
-		urls.forEach(function(item) {
-			async(item, function(result){
-				results.push(result);
-				if(results.length == urls.length) {
-					final();
-				}
-			})
-		});
+	form.parse(request, function (err, fields) {
+		processData(request, response, html, fields, 'web');
 	})
 }
 
-function sendResponse(response, list){
-	//response.end('It Works!! Path Hit: ' + request.url);
-	response.setHeader("Content-Type", "text/html");
-	var html = JSON.stringify(list);
-	//response.data = list;
-	response.end(html);
+function processData(request, response, html, fields, mode){
+	
+	var cheerio = require('cheerio'); // Basically jQuery for node.js
+	if(mode != 'email'){
+		response.write(prep.set(html, fields)); //fill in the form fields from previous request
+	}
+	html = '<div class="container"><h2>Search Results: '+fields.park.split("|")[1]+'</h2><!-- RESULTS --></div>';
+
+	var formErrors = [];
+	//bad form entries
+	if(fields.startDate == ""){
+		formErrors.push("When, oh when!");
+	}
+	if(fields.nights < 1){
+		fields.nights = "1";
+	}
+	if(fields.week < 1){
+		fields.week = "1"
+	}
+	if(formErrors.length > 0){
+		html = prep.setResults(html, formErrors.join(""));
+		response.write(html);
+		response.end();
+		return;
+	}
+	var d = new Date(fields.startDate);
+
+	var options = {
+			uri: BASE + 'place_id=' + fields.park.split("|")[0] + '&arrivalDate=' + fields.startDate + '&nights=' + fields.nights,
+			headers: {
+				'User-Agent': 'request'
+			},
+			transform: function (body) {
+				return cheerio.load(body);
+			},
+			loc: fields.park.split("|")[1],
+			date: fields.startDate,
+			day: d.getDay(),
+			nights: Number(fields.nights)
+	};
+	//url params are getting clobbert
+	options.exturl = options.uri;
+
+	var urls = [Object.assign({}, options)];
+
+	//no need to process if only 1
+	for (var i = fields.week - 1; i > 0; i--) {
+		d.setDate(d.getDate() + 7);
+		var nextDate = (d.getMonth()+1)+'/'+ d.getDate() +'/'+d.getFullYear();
+		options.uri = BASE + 'place_id=' + fields.park.split("|")[0] + '&arrivalDate=' + nextDate + '&nights=' + fields.nights;
+		options.loc = fields.park.split("|")[1];
+		options.date = (d.getMonth()+1)+'/'+ d.getDate() +'/'+d.getFullYear();
+		options.day = d.getDay();
+		options.exturl = options.uri;
+		//clone the object
+		urls.push(rp(Object.assign({}, options)));
+	}
+	// console.log('options', urls);
+
+
+	/*now loop through all the urls and get the data*/
+	var listings = [];
+
+	/*process all calls in parallel*/
+	function async(options, callback) {
+		rp(options)
+		.then(function ($) {
+			var list = [];
+			//console.log($.html());
+			$('.UnitresultDiv').find('div > a').each(function (index, element) {
+				list.push($(element).text());
+			});
+			listings.push(view.render(options, list));
+			callback(options);
+		})
+		.catch(function (err) {
+			// Crawling failed or Cheerio choked...
+			console.log('trouble', err.name, err.statusCode); //full text: err.error
+			var os = {loc: options.loc, day: options.day, date: options.date};
+			listings.push(view.render(os));
+			results.push({}); //results don't increase without this. Not sure why
+			//console.log('results count', results.length);
+			if(results.length == urls.length){
+				html = prep.setResults(html, listings.join(""));
+				if(mode != 'email'){
+					response.write(html);
+				}
+			}
+		});
+	}
+	function final() { 
+		console.log('Done', results.length);
+		html = prep.setResults(html, listings.join(""));
+		if(mode == 'email'){
+			sendEmail(html, options);
+			console.log('email send');
+			if(response){
+				response.end('Blastomated');
+			}
+		} else {
+			response.write(html);
+			response.end();
+		}
+	}
+
+	var results = [];
+
+	urls.forEach(function(item) {
+		async(item, function(result){
+			results.push(result);
+			if(results.length == urls.length) {
+				final();
+			}
+		})
+	});
+}
+
+function sendEmail(html, data){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: process.env.EMAIL_ADDRESS, // Your email id
+			pass: process.env.EMAIL_LWT // Your password
+		}
+	});
+
+	var mailOptions = {
+		from: process.env.EMAIL_ADDRESS, // sender address
+		to: process.env.EMAIL_RECIPIENT_ADDRESS, // list of receivers
+		subject: 'Campsite availability for ' + data.loc, // Subject line
+		html: html //, // plaintext body
+		// html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+	};
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			//res.json({yo: 'error'});
+		}else{
+			console.log('Message sent: ' + info.response);
+			//res.json({yo: info.response});
+		};
+	});
+}
+
+/*Scheduling jobs*/
+var rule = new schedule.RecurrenceRule();
+if(process.env.SCHEDULED_PARKS && process.env.SCHEDULED_PARKS != ""){
+	if(process.env.LOCALENV == 'true'){
+		rule.second = 30;
+	} else {
+		rule.hour = 0; //midnight
+	}
+	schedule.scheduleJob(rule, function(){
+		var req, res;
+		var d = new Date();
+		if(d.getDay() < 5){
+		  d.setDate(d.getDate() + (5 - d.getDay()));
+		} else if(d.getDay() > 5){
+		  d.setDate(d.getDate() + 6);
+		}
+		var params = {
+			week: 4,
+			nights: 2,
+			startDate: (d.getMonth()+1)+'/'+ d.getDate() +'/'+d.getFullYear()
+		}
+		var emails = process.env.SCHEDULED_PARKS.split(",");
+		emails.forEach(function(park){
+			params.park = park;
+			outputEmail(req,res,"", params);
+		})
+	});
 }
 
 
